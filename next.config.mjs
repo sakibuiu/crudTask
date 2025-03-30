ff1f64c1e645f76904dff92f12a32d84503d0798
Ade <joelayomide35@gmail.com>
2025-03-30 01:16:17 +0100

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
